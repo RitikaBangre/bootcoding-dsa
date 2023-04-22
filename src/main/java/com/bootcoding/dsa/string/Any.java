6c9384784ce5680e7bc8bc046fc079949b350b14
RitikaBangre <ritikabangre2402@gmail.com>
2023-04-22 19:41:33 +0530

    problems on String

---
+++ src/main/java/com/bootcoding/dsa/string/Any.java
@@ -0,0 +1,11 @@
+package com.bootcoding.dsa.string;
+
+public class Any {
+    public static void main(String[] args) {
+
+
+        System.out.println(" I'm alright");
+        System.out.println("I am Software Engineer in \"Google\"");
+        System.out.println("14\\04\\2023");
+    }
+}
---
+++ src/main/java/com/bootcoding/dsa/string/StringEx1.java
@@ -0,0 +1,13 @@
+package com.bootcoding.dsa.string;
+
+public class StringEx1 {
+    public static void main(String[] args) {
+
+
+        String s1 = "Diwali";
+        String s2 = "diwali";
+        boolean res = s1.equals(s2);
+        String s3 = s1.toLowerCase();
+        String s4 = s2.toUpperCase();
+    }
+}
